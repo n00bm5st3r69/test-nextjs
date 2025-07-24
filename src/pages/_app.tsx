4f48ea346ae04f871fe513d3604097ba4103328c
DESKTOP-0QSP0N5\63977 <crisostomokirk@gmail.com>
2025-07-24 09:51:56 +0800

    page layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@/components/Toolbar";
+import { Box } from "@mui/material";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function RecipeApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <CssBaseline />
+      <Box display="flex" flexDirection="column" minHeight="100vh">
+        <Toolbar />
+        <Box
+          component="main"
+          flexGrow={1}
+          overflow="auto"
+          display="flex"
+          flexDirection="column"
+        >
+          <Component {...pageProps} />
+        </Box>
+      </Box>
+    </Provider>
+  );
 }
