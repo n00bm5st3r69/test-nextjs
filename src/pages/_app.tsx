571848affdcc114685dc035e9772cfbecd1ff754
DESKTOP-0QSP0N5\63977 <crisostomokirk@gmail.com>
2025-07-25 12:25:46 +0800

    Added snackbar wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import { SnackbarProvider } from "notistack";
 import { store } from "@/store/store";
 import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@/components/Toolbar";
@@ -10,18 +11,20 @@ export default function RecipeApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <CssBaseline />
-      <Box display="flex" flexDirection="column" minHeight="100vh">
-        <Toolbar />
-        <Box
-          component="main"
-          flexGrow={1}
-          overflow="auto"
-          display="flex"
-          flexDirection="column"
-        >
-          <Component {...pageProps} />
+      <SnackbarProvider>
+        <Box display="flex" flexDirection="column" minHeight="100vh">
+          <Toolbar />
+          <Box
+            component="main"
+            flexGrow={1}
+            overflow="auto"
+            display="flex"
+            flexDirection="column"
+          >
+            <Component {...pageProps} />
+          </Box>
         </Box>
-      </Box>
+      </SnackbarProvider>
     </Provider>
   );
 }
