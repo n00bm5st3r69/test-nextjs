a869e0e7cdf1542c5569c88d229a69060534d254
DESKTOP-0QSP0N5\63977 <crisostomokirk@gmail.com>
2025-07-25 12:29:34 +0800

    formatted the validator file

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,13 +1,13 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const recipeSchema = z.object({
-  name: z.string().min(1, 'Name is required'),
-  email: z.email('Invalid email'),
-  title: z.string().min(1, 'Title is required'),
-  description: z.string().min(1, 'Description is required'),
-  ingredients: z.string().min(1, 'Ingredients are required'),
-  instructions: z.string().min(1, 'Instructions are required'),
+  name: z.string().min(1, "Name is required"),
+  email: z.email("Invalid email"),
+  title: z.string().min(1, "Title is required"),
+  description: z.string().min(1, "Description is required"),
+  ingredients: z.string().min(1, "Ingredients are required"),
+  instructions: z.string().min(1, "Instructions are required"),
   image: z.any().refine((file) => file?.length > 0, {
-    message: 'Image is required',
+    message: "Image is required",
   }),
 });
